fa8825d60c2c6f49c200467844607f8c178bb93c
johnwallm <64194445+johnwallm@users.noreply.github.com>
2020-09-12 02:43:36 +0800

    Reupdated

--- SubmitBE/Controllers/HomeController.cs
+++ SubmitBE/Controllers/HomeController.cs
@@ -1,34 +1,29 @@
 ﻿using SubmitBE.Models;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Security;
-using System.Web.UI.WebControls;
-
-
 
 namespace SubmitBE.Controllers
 {
     public class HomeController : Controller
     {
      
-        
+        private TeraEntities3 db = new TeraEntities3();
         //
         //FOR GETTING THE IMAGE OR FOR PROMPTING THE PAGE WITH CHOOSE FILE
         //
 
         
-        int UserInformationID = 4;
-        string CaseLocation = "makati";
-        int EnvironmentalConcernID = 2;
-        string XCoordinates = "1";
-        string YCoordinates = "2";
-        int UpdatedStatusID = 4;
-        string Notes = "Water";
-        int VolunteerID = 4;
+        //int UserInformationID = 4;
+        //string CaseLocation = "makati";
+        //int EnvironmentalConcernID = 2;
+        //string XCoordinates = "1";
+        //string YCoordinates = "2";
+        //int UpdatedStatusID = 4;
+        //string Notes = "Water";
+        //int VolunteerID = 4;
 
         public ActionResult AddImage()
         {
@@ -39,13 +34,13 @@ namespace SubmitBE.Controllers
         //FOR GETTING THE IMAGE OR FOR PROMPTING THE PAGE WITH CHOOSE FILE
         //
         [HttpPost]
-        public ActionResult AddImage(CaseReport model, HttpPostedFileBase imgInp)
+        public ActionResult AddImage(CaseReport model, HttpPostedFileBase image1)
         {
             var db = new TeraEntities3();
-            if (imgInp!= null)
+            if (image1!= null)
             {
-                model.CaseReportPhoto = new byte[imgInp.ContentLength];
-                imgInp.InputStream.Read(model.CaseReportPhoto, 0, imgInp.ContentLength);
+                model.CaseReportPhoto = new byte[image1.ContentLength];
+                image1.InputStream.Read(model.CaseReportPhoto, 0, image1.ContentLength);
             }
 
             db.CaseReports.Add(model);
@@ -56,7 +51,7 @@ namespace SubmitBE.Controllers
         //for image retrieving OR SHOWING
         public ActionResult GetImage()
         {
-            return View();
+            return View(db.CaseReports.ToList());
         }
     }
 }
